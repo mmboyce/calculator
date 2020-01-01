f22f41c112f044f222f7a450bb308b34109e6402
mmboyce <mathieumimmsboyce@gmail.com>
2020-01-01 12:44:16 -0500

    Add pressed buttons to the display.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const display = document.querySelector("#display")
+const nums = document.querySelectorAll('.num')
+const funcs = document.querySelectorAll('.func')
+const clear = document.querySelector("#clear")
+
 const add = (x, y) => x + y
 
 const subtract = (x, y) => x - y
@@ -9,16 +14,44 @@ const divide = (x, y) => x / y
 const operate = (operator, x, y) => {
     switch (operator) {
         case '+':
-            return add(x,y)
+            return add(x, y)
         case '-':
-            return subtract(x,y)
+            return subtract(x, y)
         case 'x':
         case '*':
-            return multiply(x,y)
+            return multiply(x, y)
         case '/':
         case '÷':
-            return divide(x,y)
+            return divide(x, y)
         default:
             break;
     }
 }
+
+const addToDisplay = (toAdd) => {
+    display.value += toAdd
+}
+
+// add eventListener to add nums to display
+nums.forEach(num => {
+    num.addEventListener("click", () => {
+        let numId = num.innerText
+        addToDisplay(numId)
+    })
+})
+
+// clear button functionality
+clear.addEventListener("click", () => {
+    display.value = ""
+})
+
+
+// function buttons add to display (except equals)
+funcs.forEach(func => {
+    if (func.id != "func-equal") {
+        let funcID = func.innerText
+        func.addEventListener("click", () => {
+            addToDisplay(" " + funcID + " ")
+        })
+    }
+})
